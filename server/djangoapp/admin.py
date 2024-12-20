36f871925df80e83d1c2c63fe4517d49b1f76ca4
IlCodeLover <ilcodelover@gmail.com>
2024-12-20 09:38:52 +0100

    [Mod3] create CarModel and CarMaker and register them into Admin site so that we can do CRUD manually

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,9 +1,10 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarModel, CarMake
 
 
-# Register your models here.
-
+# TO DO: Register your models here. Afterward, you will see your models appearing on localhost:8080/admin
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 # CarModelInline class
 
 # CarModelAdmin class
